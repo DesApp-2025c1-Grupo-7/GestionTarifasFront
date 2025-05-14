e132dbf33707757e84cfb13fc6508fb1fd8cf75a
leorodriguez99 <leorodriguez8899@gmail.com>
2025-05-14 20:46:15 -0300

    estructura de la pagina principal

---
+++ gestion-de-tarifas-front/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <nav>
+        <ul>
+           
+            <li><button className='boton'> Registrar tarifa </button></li>
+            <li><button className='boton'> Historico de tarifas </button></li>
+        </ul>
+      
+    </nav>
+  )
+}
+
+export default Header
